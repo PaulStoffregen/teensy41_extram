e940b90b1c089512da2c4bbdf3ecacca8c71312e
Defragster <Defragster@users.noreply.github.com>
2020-03-12 02:04:05 -0700

    Add #5 Read loopTest3R

--- extRAM_SPIFFS_t4/examples/eram_spiffs_partv2/eram_spiffs_partv2.ino
+++ extRAM_SPIFFS_t4/examples/eram_spiffs_partv2/eram_spiffs_partv2.ino
@@ -12,7 +12,7 @@
 // do dtSTART; and dtEND( "what") to show time
 elapsedMicros _dt;
 #define dtSTART {_dt=0;}
-#define dtEND(a) { Serial.printf( "\n%s()_%s : dt %ul us", __func__, a, (uint32_t)_dt);}
+#define dtEND(a) { Serial.printf( "\n%s()_%s : dt %lu us", __func__, a, (uint32_t)_dt);}
 // do dtSTART; and dtEND( "what") to show time
 
 #include <extRAM_t4.h>
@@ -22,7 +22,7 @@ extRAM_t4 eRAM;
 
 uint8_t config = 0; //0 - init eram only, 1-init flash only, 2-init both
 uint8_t spiffs_region = 2;  //0 - flash, 1 - eram
-                            //2 - 2 4meg eram pseudo partitions
+//2 - 2 4meg eram pseudo partitions
 //#define DO_DEBUG 1
 
 char buf[512] = "Hello World! What a wonderful World :)";
@@ -83,9 +83,10 @@ void setup() {
   Serial.println();
   Serial.println("Mount SPIFFS:");
   eRAM.fs_mount();
+  loopTest3R();
   Serial.println("Directory contents:");
   eRAM.fs_listDir();
-  
+
   //test of print
   Serial.println();
   Serial.println("Using println and printf to printoutput file");
@@ -99,7 +100,7 @@ void setup() {
   Serial.println();
   Serial.println("Test print output:");
   eRAM.f_open("PRINTOUTPUT", SPIFFS_RDONLY);
-  eRAM.f_read(buf,512);
+  eRAM.f_read(buf, 512);
   eRAM.f_close();
   Serial.println(buf);
   Serial.println("==================================================");
@@ -110,7 +111,7 @@ void loop() {
   char chIn = 255;
   if ( Serial.available() ) {
     do {
-      if ( chIn < '0' || chIn > '4' )
+      if ( chIn < '0' || chIn > '5' )
         chIn = Serial.read();
       else
         Serial.read();
@@ -143,6 +144,11 @@ void loop() {
     check24();
     Serial.println();
   }
+  else if ( chIn == '5' ) {
+    Serial.println("\n5 :: Loop Test5 - READ ONLY Verify Test");
+    loopTest3R();
+    Serial.println();
+  }
 }
 
 void loopTest2() {
@@ -151,12 +157,12 @@ void loopTest2() {
   char fname1[52] = "loopTest2";
 
   /**
-   * Opens/creates a file.
-   * @param fs      the file system struct
-   * @param path    the path of the new file
-   * @param flags   the flags for the open command, can be combinations of
-   *                SPIFFS_APPEND, SPIFFS_TRUNC, SPIFFS_CREAT, SPIFFS_RDONLY,
-   *                SPIFFS_WRONLY, SPIFFS_RDWR, SPIFFS_DIRECT, SPIFFS_EXCL
+     Opens/creates a file.
+     @param fs      the file system struct
+     @param path    the path of the new file
+     @param flags   the flags for the open command, can be combinations of
+                    SPIFFS_APPEND, SPIFFS_TRUNC, SPIFFS_CREAT, SPIFFS_RDONLY,
+                    SPIFFS_WRONLY, SPIFFS_RDWR, SPIFFS_DIRECT, SPIFFS_EXCL
   **/
   dtSTART;
   eRAM.f_open(fname1, SPIFFS_CREAT | SPIFFS_TRUNC | SPIFFS_RDWR);
@@ -273,8 +279,8 @@ void loopTest3() {
   eRAM.f_close();
   dtEND( "write");
 
-//  Serial.println("Directory contents:");
-//  eRAM.fs_listDir();
+  //  Serial.println("Directory contents:");
+  //  eRAM.fs_listDir();
 
   eRAM.f_open(fname1, SPIFFS_RDONLY);
   //if (SPIFFS_read(&fs, fd, (u8_t *)xData1, 2600) < 0) Serial.printf("errno %i\n", SPIFFS_errno(&fs));
@@ -322,6 +328,37 @@ void loopTest3() {
     jj += 26;
   }
 #endif
+}
+
+void loopTest3R() {
+  char xData[12048], xData1[12048];
+  Serial.println("Loop Test 3R -  READ ONLY Verify");
+
+  char fname1[32] = "loopTest3";
+  dtSTART
+
+  eRAM.f_open(fname1, SPIFFS_RDONLY);
+  //if (SPIFFS_read(&fs, fd, (u8_t *)xData1, 2600) < 0) Serial.printf("errno %i\n", SPIFFS_errno(&fs));
+  eRAM.f_read(xData1, 2600);
+  //SPIFFS_close(&fs, fd);
+  eRAM.f_close();
+  dtEND( "5 :: Loop Test5 - READ ONLY Verify Test");
+
+  for ( int ii = 0; ii < 100; ii++) {
+    for ( int jj = 0; jj < 26; jj++) {
+      if ( ii % 2 )
+        xData[jj + (ii * 26)] = 'A' + jj;
+      else
+        xData[jj + (ii * 26)] = 'a' + jj;
+    }
+  }
+  if ( memcmp( &xData, &xData1, 2600) ) {
+    Serial.print("\t:( - Content not in ERAM");
+  }
+  else {
+    Serial.print("\t:) - Expected Data in ERAM");
+  }
+  Serial.print('\n');
 }
 
 #if 0
